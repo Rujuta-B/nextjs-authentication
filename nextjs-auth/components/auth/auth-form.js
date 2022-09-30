20c6d4596ec303ea46f753b61ee87f07730b2753
brujuta <97433576+Rujuta-B@users.noreply.github.com>
2022-09-30 16:33:57 +0530

    Add files via upload

--- nextjs-auth/components/auth/auth-form.js
+++ nextjs-auth/components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/client";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -32,6 +33,14 @@ function AuthForm() {
     const passwordInputValue = enteredPasswordRef.current.value;
 
     if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: emailInputValue,
+        password: passwordInputValue,
+      });
+      // console.log(result);
+      if (!result.error) {
+      }
     } else {
       try {
         const result = await createUser(emailInputValue, passwordInputValue);
